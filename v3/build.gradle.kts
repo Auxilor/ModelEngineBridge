9c193dc66a3f98e74f50b9075b8bd3ff57da225e
Will FP <william.favierparsons1@gmail.com>
2024-01-06 11:25:55 +0000

    I hate you, model engine

--- v3/build.gradle.kts
+++ v3/build.gradle.kts
@@ -3,6 +3,6 @@ version = rootProject.version
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
-    compileOnly("com.ticxo.modelengine:api:R3.1.9")
+    compileOnly("com.ticxo.modelengine:api:R3.1.8")
     compileOnly(project(":api"))
 }
