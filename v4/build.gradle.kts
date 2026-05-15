7c2c513d214950c315324ab0b8c745b0212e9ed8
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-05-15 23:50:02 +0100

    Update dependencies and improve ModelEngine integration (#3)

--- v4/build.gradle.kts
+++ v4/build.gradle.kts
@@ -2,7 +2,7 @@ group = "com.willfp"
 version = rootProject.version
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
-    compileOnly("com.ticxo.modelengine:ModelEngine:R4.0.3")
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    compileOnly("com.ticxo.modelengine:ModelEngine:R4.0.4")
     compileOnly(project(":api"))
 }
